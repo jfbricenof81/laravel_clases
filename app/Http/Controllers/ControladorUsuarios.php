62339e4f285761bc65fbb801a5fad85ce7a00cab
jfbricenof81 <julianbricenoflorezsena@gmail.com>
2024-10-01 14:19:19 -0500

    creacion ruta api

---
+++ app/Http/Controllers/ControladorUsuarios.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ControladorUsuarios extends Controller
+{
+    public function control()
+           {
+                 return 'Julian, mensaje por medio del controlador';
+            }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
+
+require __DIR__.'/auth.php';
